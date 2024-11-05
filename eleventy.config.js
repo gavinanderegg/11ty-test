17a51a2bc5364ab7361f9b030895447078f7e628
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2024-11-05 12:53:57 -0400

    Putting posts in the right place
    ... after removing the time from the date

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,11 +3,16 @@ import { DateTime } from "luxon";
 export default async function(eleventyConfig) {
 
 	// Date handling
-	eleventyConfig.addDateParsing(function(dateValue) {
-		if (dateValue !== undefined) {
-			return DateTime.fromFormat(dateValue, "yyyy-MM-dd hh:mm:ss z");
-		} else {
-			return dateValue;
-		}
-	});
+	// eleventyConfig.addDateParsing(function(dateValue) {
+	// 	if (dateValue !== undefined) {
+	// 		console.log('defined');
+	// 		console.log(dateValue);
+	// 		console.log(DateTime.fromFormat(dateValue, "yyyy-MM-dd"));
+	// 		// return DateTime.fromFormat(dateValue, "yyyy-MM-dd");
+	// 	} else {
+	// 		console.log('UNdefined');
+	// 		console.log(dateValue);
+	// 		return dateValue;
+	// 	}
+	// });
 };
--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,7 +1,14 @@
 export default {
 	permalink: function (data) {
-		// console.log(data);
+		console.log(data.page.date);
 
-		return `/${data.page.date.getFullYear()}/${data.page.date.month}/${data.page.date.day}/${data.page.fileSlug}`;
+		let day = data.page.date.getUTCDate();
+		let month = data.page.date.getUTCMonth() + 1;
+		let year = data.page.date.getFullYear();
+
+		let pDay = day.toString().padStart(2, '0');
+		let pMonth = month.toString().padStart(2, '0');
+
+		return `/${year}/${pMonth}/${pDay}/${data.page.fileSlug}.html`;
 	},
 };
